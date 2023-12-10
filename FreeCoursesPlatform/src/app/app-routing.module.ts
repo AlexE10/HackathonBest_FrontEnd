f2ea1dcba1b8480d06db5b1e9efa844e82f6a44c
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2023-12-10 02:45:27 +0200

    Created route for register

--- FreeCoursesPlatform/src/app/app-routing.module.ts
+++ FreeCoursesPlatform/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Import your components
 import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'logIn', component: LogInComponent },
+  { path: 'register', component: RegisterComponent },
+
   // more routes...
 ];
 
